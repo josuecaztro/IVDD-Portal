c49fefffa84696c7135440cacee9971ab617c518
Josue Castro <josuecaztro@gmail.com>
2024-10-13 21:58:24 -0400

    started template boilerplate

---
+++ ivdd-portal/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div id="full-portal-page">
+     <h4>Iglesia Voz de Dios</h4>
+     <h1>Admin Portal</h1>
+    </div>
+  );
+}
+
+export default App;
